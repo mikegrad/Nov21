dd8e3e50e88ee7123f11fd65ba18855b84638edf
mikegrad <mikegrad@yahoo.com>
2013-11-21 14:17:28 -0500

    Initial Commit

---
+++ Nov21/Nov21AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Nov21AppDelegate.h
+//  Nov21
+//
+//  Created by Michael Gradilone on 11/21/13.
+//  Copyright (c) 2013 FintechSolutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Nov21AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Nov21/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Nov21
+//
+//  Created by Michael Gradilone on 11/21/13.
+//  Copyright (c) 2013 FintechSolutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Nov21AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Nov21AppDelegate class]));
+    }
+}
